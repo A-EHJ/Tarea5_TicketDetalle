84aabac76dc819438ce4196d5ee4b7e4df6761ae
A-EHJ <abrahamelhage2003@gmail.com>
2023-10-22 14:15:53 -0400

    Creación de modelos

---
+++ Shared/Tickets.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TicketDetalle.Shared
+{
+    public class Tickets
+    {
+        [Key]
+        public int TicketId { get; set; }
+
+        public DateTime Fecha { get; set; } = DateTime.Now;
+
+        [Required(ErrorMessage = "El Asunto es requerido")]
+        public string? Asunto { get; set; }
+
+        [ForeignKey("TicketId")]
+        public ICollection<TicketsDetalle> TicketsDetalle { get; set; } = new List<TicketsDetalle>();
+    }
+}
